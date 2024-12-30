51746ef04c0d83e8b6d113027589db039ee9563c
Raphael Nestler <raphael.nestler@renuo.ch>
2024-12-30 18:00:53 +0100

    Add some weird animations :)

--- FirmwareExamples/RotatedText/src/main.cpp
+++ FirmwareExamples/RotatedText/src/main.cpp
@@ -11,19 +11,19 @@
 
 #define LED_MATRIX_WIDTH 19
 #define LED_MATRIX_HEIGHT 11
-Adafruit_NeoMatrix *matrix = new Adafruit_NeoMatrix(LED_MATRIX_WIDTH, LED_MATRIX_HEIGHT, 
+Adafruit_NeoMatrix *matrix = new Adafruit_NeoMatrix(LED_MATRIX_WIDTH, LED_MATRIX_HEIGHT,
   1, 2, // rows, cols - we use 2 cols to get enough LEDs for the scarf
   LED_PIN,
   NEO_MATRIX_TOP     + NEO_MATRIX_LEFT +
-    NEO_MATRIX_ROWS + NEO_MATRIX_PROGRESSIVE + 
+    NEO_MATRIX_ROWS + NEO_MATRIX_PROGRESSIVE +
 // progressive vs zigzag makes no difference for a 4 arrays next to one another
     NEO_TILE_TOP + NEO_TILE_LEFT +  NEO_TILE_PROGRESSIVE,
   NEO_GRB            + NEO_KHZ800 );
 
-void setScarfTop();
-void setScarfBottom();
-void setEye();
-void setHorn();
+void setScarfTop(uint8_t intensity);
+void setScarfBottom(uint8_t intensity);
+void setEye(uint8_t intensity);
+void setHorn(uint8_t intensity);
 void setMatrix();
 
 void setup() {
@@ -37,6 +37,22 @@ void setup() {
   matrix->show();
 }
 int pos=0;
+int red = 128;
+int green = 128;
+int blue = 128;
+
+int limit_u8(int value, uint8_t min = 10, uint8_t max = 255) {
+    if (value < min) {
+        return min;
+    }
+
+    if (value > max) {
+        return max;
+    }
+
+    return value;
+}
+
 void loop() {
     pos++;
 
@@ -46,10 +62,22 @@ void loop() {
     setMatrix();
 
     // set eye, horn, scarfs
-    setEye();
-    setHorn();
-    setScarfTop();
-    setScarfBottom();
+    if (pos % 137 != 0) {
+        setScarfTop(128);
+    }
+    if (pos % 27 != 0) {
+        setScarfBottom(128);
+    }
+
+    if (pos % 10 == 0) {
+        setEye(255);
+    } else if (pos % 5 == 0) {
+        setHorn(150);
+    }
+
+    red = limit_u8(red + random(-5, 6));
+    green = limit_u8( green + random(-5, 6));
+    blue = limit_u8(blue + random(-5, 6));
 
     matrix->show();
     delay(50);
@@ -71,8 +99,8 @@ void setMatrix()
         matrix->setTextWrap(false);
         matrix->setTextSize(1);
         matrix->setRotation(0);
-        matrix->setTextColor(matrix->Color(255,0,255));
-        matrix->print("LuxCamp 2024");
+        matrix->setTextColor(matrix->Color(red,green,blue));
+        matrix->print("rnstlr 38c3");
     }
 
     // vertical text
@@ -83,37 +111,37 @@ void setMatrix()
         matrix->setTextWrap(false);
         matrix->setTextSize(fontSize);
         matrix->setRotation(3);
-        matrix->setTextColor(matrix->Color(255,0,255));
-        matrix->print("Spit Happens!");
+        matrix->setTextColor(matrix->Color(red,green,blue));
+        matrix->print("Teufelslama!");
     }
 }
 
-void setEye()
+void setEye(uint8_t intensity=255)
 {
-    matrix->setPixelColor(EYE, 255,0,0);
+    matrix->setPixelColor(EYE, intensity,0,0);
 }
 
-void setHorn()
+void setHorn(uint8_t intensity=255)
 {
-    matrix->setPixelColor(HORN, 50,100,150);   
+    matrix->setPixelColor(HORN, intensity / 3,intensity * 2 / 3,intensity);
 }
 
-void setScarfBottom()
+void setScarfBottom(uint8_t intensity)
 {
     int ledsScarfBottom[7] = {16,17,18,37,170,189,190};
 
     for(int i : ledsScarfBottom)
     {
-        matrix->setPixelColor(i,255,0,255);
+        matrix->setPixelColor(i,intensity,0,intensity);
     }
 }
 
-void setScarfTop()
+void setScarfTop(uint8_t intensity=255)
 {
     int ledsScarfTop[7] = {207,208,209,210,211,212,213};
 
     for(int i : ledsScarfTop)
     {
-        matrix->setPixelColor(i,255,0,255);
+        matrix->setPixelColor(i,intensity,0,intensity);
     }
 }
